cb56df62633b21dd1e8fe834b2eafe53edb4b86f
root <sivertlo@stud.ntnu.no>
2018-11-20 23:49:17 +0100

    Refactored encoder thread to be runnable and use write to storage box

--- src/main/java/sdv/devices/motor/EncoderThread.java
+++ src/main/java/sdv/devices/motor/EncoderThread.java
@@ -1,26 +1,41 @@
 package sdv.devices.motor;
 
+import sdv.algorithms.tools.StorageBox;
 import sdv.networking.motor.TcpClient;
 
 
-public class EncoderThread extends Thread{
-    private TcpClient pythonClient = null;
-    private MotorCommands motorCommands = null;
+public class EncoderThread implements Runnable{
+    private TcpClient pythonClient;
+    private MotorCommands motorCommands;
+    private StorageBox box;
 
-    public EncoderThread(TcpClient tcpClient, MotorCommands motorCommands){
+
+    public EncoderThread(TcpClient tcpClient, MotorCommands motorCommands, StorageBox box){
         this.pythonClient = tcpClient;
         this.motorCommands = motorCommands;
+        this.box = box;
     }
 
     @Override
     public void run(){
+        while(true)
+            if(box.getRun()) {
+                box.setValue(getEncoder());
+                box.setRun(false);
+                box.setRead(true);
+            }
     }
+
+    /**
+     *
+     * @return
+     **/
 
     public String[] getEncoder() {
         String[] strings;
         motorCommands.getEncoderData();
         pythonClient.messageFromServer();
-        if (!pythonClient.response[1].contains("FAILED") && !pythonClient.response[3].contains("FAILED")) {
+        if(!pythonClient.response[1].contains("FAILED") && !pythonClient.response[3].contains("FAILED")) {
             //System.out.println("Encoder1: " + pythonClient2.response[1]);
             //System.out.println("Encoder2: " + pythonClient2.response[3]);
             strings = pythonClient.response;
