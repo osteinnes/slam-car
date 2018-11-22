7e0cc5b0a893c98c8ce4017cc248cc2fffb5307b
Ole-martin Steinnes <omsteinn@stud.ntnu.no>
2018-11-22 19:29:40 +0100

    Added comments to the LidarScan-class

--- src/main/java/sdv/devices/lidar/LidarScan.java
+++ src/main/java/sdv/devices/lidar/LidarScan.java
@@ -7,14 +7,39 @@ import sdv.tools.boxes.LidarBox;
 import java.util.List;
 import java.util.Vector;
 
+/**
+ * LidarScan handles the scanning by the lidar in a seperate
+ * thread than the rest of the program. Since the LiDAR and SLAM
+ * is not compatible we need to enter each sample of the LiDAR scan.
+ * This results in us entering each of the 1000 samples in one revolution,
+ * fetching the distance point and place them in a int-array which represents
+ * a full revolution (360 degrees) and sends this array to the StorageBox
+ * for the SLAM-algorithm to use.
+ *
+ * @author Ole-martin Steinnes
+ */
 public class LidarScan extends Thread {
 
+    // Lidar instance
     private SweepDevice sweepDevice;
+
+    // StorageBox instance made for LiDAR
     private LidarBox lidarBox;
+
+    // Number of expected samples per revolution
     private int sampleLimit;
+
+    // Number of scans contained by the Vector<>
     private int ns;
 
-
+    /**
+     * Constructor of the LidarScan-class. Creates accepts instances
+     * of SweepDevice, LidarBox and the samplelimit for one revolution.
+     *
+     * @param sweepDevice   LiDAR-instance
+     * @param lidarBox      StorageBox-instance
+     * @param sampleLimit   Limit of samples per revolution
+     */
     public LidarScan(SweepDevice sweepDevice, LidarBox lidarBox, int sampleLimit) {
         this.sweepDevice = sweepDevice;
         this.lidarBox = lidarBox;
@@ -68,6 +93,9 @@ public class LidarScan extends Thread {
         }
     }
 
+    /**
+     * Stops the LiDAR-scan and stops the LiDAR's motor.
+     */
     public void stopLidar() {
         sweepDevice.stopScanning();
         sweepDevice.setMotorSpeed(0);
